773422a123ffb7edea7872507d1fdf333dbcb322
LNA02 <leenhatanh.2002@gmail.com>
2024-05-23 23:37:28 +0700

    change recommend

--- backend_python/app.py
+++ backend_python/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # API endpoints
-TOUR_API = 'https://6645c30ab8925626f89310d5.mockapi.io/api/v1/tour'
-RATINGS_API = 'https://6645c30ab8925626f89310d5.mockapi.io/api/v1/rating'
+TOUR_API = 'http://localhost:8080/api/v1/tour'
+RATINGS_API = 'http://localhost:8080/api/v1/feedback/all'
 
 # Function to get data from API
 def fetch_data(api_url):
@@ -25,11 +25,17 @@ ratings_df = fetch_data(RATINGS_API)
 tour_df['tourId'] = tour_df['tourId'].astype(str)
 ratings_df['tourId'] = ratings_df['tourId'].astype(str)
 
+
+# Trim tour_df to keep only 'tourId', 'tourName', and 'categories' columns
+tour_df = tour_df[['tourId', 'tourName', 'categories','thumbnail']]
+ratings_df = ratings_df[['tourId', 'userId', 'rating']]
+
+
 # Convert categories to pipe-separated string format
-tour_df['categories'] = tour_df['categories'].apply(lambda x: '|'.join(x))
+tour_df['categories'] = tour_df['categories'].apply(lambda x: '|'.join([d['categoryName'] for d in x]))
 
 # Convert rating to a decimal format
-ratings_df['rating'] = ratings_df['rating'] / 10.0
+# ratings_df['rating'] = ratings_df['rating'] / 10.0
 
 # Merge the dataframes on 'tourId'
 df = tour_df.merge(ratings_df, on='tourId')
@@ -45,25 +51,22 @@ def recommend():
     tour_name = request.args.get('tourName')
     
     if not tour_name:
-        tour_name = 'tourName 5'
+        tour_name = 'hue tour'
     
     recommended_tours = []
 
     tour_db = df[df['tourName'] == tour_name].sort_values(by='rating', ascending=False)
-    print(tour_db)
 
     for user in tour_db.iloc[:5]['userId'].values:
         rated_tours = df[df['userId'] == user]
         rated_tours = rated_tours[rated_tours['tourName'] != tour_name].sort_values(by='rating', ascending=False).iloc[:5]
-        recommended_tours.extend(list(rated_tours['tourName'].values))
+        recommended_tours.extend(list(rated_tours[['tourName', 'tourId', 'thumbnail']].to_dict(orient='records')))
         
-    recommended_tours = np.unique(recommended_tours)
-
     gtour_categories = df[df['tourName'] == tour_name].iloc[0]['categories'].split('|')
     scores = {}
 
     for tour in recommended_tours:
-        toured = df[df['tourName'] == tour].iloc[0]
+        toured = df[df['tourName'] == tour['tourName']].iloc[0]
         tour_categories = toured['categories'].split('|')
         score = 0
         
@@ -71,11 +74,17 @@ def recommend():
             if gtour_category in tour_categories:
                 score += 1
         
-        scores[tour] = score
-        
-    recommended_tours = sorted(scores, key=lambda x: scores[x], reverse=True)
-
+        scores[tour['tourName']] = score
+    
+    sorted_tours = sorted(scores, key=lambda x: scores[x], reverse=True)
+    recommended_tours = [{'tourName': tour_name, 'tourId': df[df['tourName'] == tour_name].iloc[0]['tourId'], 'thumbnail': df[df['tourName'] == tour_name].iloc[0]['thumbnail']}]
+    for tour_name in sorted_tours:
+        tour_data = df[df['tourName'] == tour_name].iloc[0]
+        recommended_tours.append({'tourName': tour_name, 'tourId': tour_data['tourId'], 'thumbnail': tour_data['thumbnail']})
+    
     return jsonify({"recommendations": recommended_tours})
+
+
 
 @app.route('/api/hint', methods=['GET'])
 def hint():
