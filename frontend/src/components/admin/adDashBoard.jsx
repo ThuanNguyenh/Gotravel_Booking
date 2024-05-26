deefc4df3162ef08e7e74d2c7e0fef4152f852f4
Le Khanh <lekhanh@MacBook-Air-cua-Le.local>
2024-05-26 17:27:30 +0700

    adminDuy

--- frontend/src/components/admin/adDashBoard.jsx
+++ frontend/src/components/admin/adDashBoard.jsx
@@ -112,9 +112,36 @@ function AdminDashBoard() {
           {
             type: "KPI",
             renderTo: "dashboard-row-1-cell-2",
-            title: "Savings",
+            title: "Số lượng người dùng",
             value: 6500,
-            valueFormat: "$ {value}",
+            valueFormat: "",
+            subtitle: "22%",
+            linkedValueTo: {
+              enabled: false,
+            },
+            chartOptions: {
+              chart: {
+                styledMode: true,
+              },
+              series: [
+                {
+                  type: "spline",
+                  enableMouseTracking: false,
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  data: [0, 1000, 1000, 4500, 5300, 6500],
+                },
+              ],
+            },
+          },
+          // 
+          {
+            type: "KPI",
+            renderTo: "dashboard-row-1-cell-3",
+            title: "Số lượng tourt trong tháng",
+            value: 6500,
+            valueFormat: "",
             subtitle: "22%",
             linkedValueTo: {
               enabled: false,
@@ -136,34 +163,9 @@ function AdminDashBoard() {
             },
           },
           {
-            type: "HTML",
-            renderTo: "dashboard-row-1-cell-3",
-            elements: [
-              {
-                tagName: "div",
-                children: [
-                  {
-                    tagName: "h4",
-                    textContent: "Check how you can save more!",
-                    attributes: {
-                      class: "main-title align-middle",
-                    },
-                  },
-                  {
-                    tagName: "button",
-                    textContent: "Go to the saving account",
-                    attributes: {
-                      id: "saving-button",
-                    },
-                  },
-                ],
-              },
-            ],
-          },
-          {
             type: "Highcharts",
             renderTo: "dashboard-row-2-cell-1",
-            title: "Earnings",
+            title: "Tổng doanh thu",
             chartOptions: {
               chart: {
                 marginTop: 50,
@@ -236,70 +238,70 @@ function AdminDashBoard() {
               ],
             },
           },
-          {
-            type: "KPI",
-            renderTo: "dashboard-row-3-cell-1",
-            title: "Spendings",
-            value: 350,
-            valueFormat: "$ {value}",
-            linkedValueTo: {
-              enabled: false,
-            },
-            chartOptions: {
-              series: [
-                {
-                  type: "column",
-                  enableMouseTracking: false,
-                  dataLabels: {
-                    enabled: false,
-                  },
-                  name: "Spendings",
-                  data: [45, 30, 50, 80, 10, 45, 30, 59, 39, 15, 62],
-                },
-              ],
-            },
-          },
-          {
-            type: "KPI",
-            renderTo: "dashboard-row-3-cell-2",
-            title: "Your wallet condition",
-            value: "",
-            subtitle: "You saved 1450$ this month",
-            linkedValueTo: {
-              enabled: false,
-            },
-            chartOptions: {
-              title: {
-                verticalAlign: "middle",
-                floating: true,
-                text: "58%",
-              },
-              series: [
-                {
-                  type: "pie",
-                  enableMouseTracking: false,
-                  data: [58, 42],
-                  size: "100%",
-                  innerSize: "75%",
-                  dataLabels: {
-                    enabled: false,
-                  },
-                },
-              ],
-            },
-          },
-          {
-            renderTo: "dashboard-row-3-cell-3",
-            connector: {
-              id: "transactions",
-            },
-            title: "Transactions",
-            type: "DataGrid",
-            dataGridOptions: {
-              cellHeight: 33,
-              editable: false,
-            },
-          },
+          // {
+          //   type: "KPI",
+          //   renderTo: "dashboard-row-3-cell-1",
+          //   title: "Spendings",
+          //   value: 350,
+          //   valueFormat: "$ {value}",
+          //   linkedValueTo: {
+          //     enabled: false,
+          //   },
+          //   chartOptions: {
+          //     series: [
+          //       {
+          //         type: "column",
+          //         enableMouseTracking: false,
+          //         dataLabels: {
+          //           enabled: false,
+          //         },
+          //         name: "Spendings",
+          //         data: [45, 30, 50, 80, 10, 45, 30, 59, 39, 15, 62],
+          //       },
+          //     ],
+          //   },
+          // },
+          // {
+          //   type: "KPI",
+          //   renderTo: "dashboard-row-3-cell-2",
+          //   title: "Your wallet condition",
+          //   value: "",
+          //   subtitle: "You saved 1450$ this month",
+          //   linkedValueTo: {
+          //     enabled: false,
+          //   },
+          //   chartOptions: {
+          //     title: {
+          //       verticalAlign: "middle",
+          //       floating: true,
+          //       text: "58%",
+          //     },
+          //     series: [
+          //       {
+          //         type: "pie",
+          //         enableMouseTracking: false,
+          //         data: [58, 42],
+          //         size: "100%",
+          //         innerSize: "75%",
+          //         dataLabels: {
+          //           enabled: false,
+          //         },
+          //       },
+          //     ],
+          //   },
+          // },
+          // {
+          //   renderTo: "dashboard-row-3-cell-3",
+          //   connector: {
+          //     id: "transactions",
+          //   },
+          //   title: "Transactions",
+          //   type: "DataGrid",
+          //   dataGridOptions: {
+          //     cellHeight: 33,
+          //     editable: false,
+          //   },
+          // },
         ],
       },
       true
@@ -308,7 +310,7 @@ function AdminDashBoard() {
 
   return(
     <div className="w-full">
-        <div id="financeAd" className="highcharts-light"></div>;
+        <div id="financeAd" className="highcharts-light"></div>
     </div>
   )
 }
